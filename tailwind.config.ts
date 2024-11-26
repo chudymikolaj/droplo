76fc7b8c51a5b3f4046ddb8f9ed3bbfd110c7a99
chudymikolaj <chudymikolaj06@gmail.com>
2024-11-26 22:33:12 +0100

    feat: add header and empty menu box to Home and add custom variables to tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,22 @@ export default {
 	theme: {
 		extend: {
 			colors: {
+				"button-primary": "#7F56D9",
+				"button-border": "#7F56D9",
+				"background-site": "#F5F5F5",
+				"background-primary": "#F9FAFB",
+				"background-secondary": "#EAECF0",
+				"text-teritary": "#475467",
 				background: "var(--background)",
 				foreground: "var(--foreground)",
+			},
+		},
+		container: {
+			screens: {
+				sm: "600px",
+				md: "728px",
+				lg: "984px",
+				xl: "1208px",
 			},
 		},
 	},
