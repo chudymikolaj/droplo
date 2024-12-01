f14ec760faeb3d6846c0ae2e621d63ad6ccc6f6d
chudymikolaj <chudymikolaj06@gmail.com>
2024-12-01 20:04:21 +0100

    feat: add app context to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
+"use client";
+
+import { AppProvider } from "@/context/AppContext";
 import ButtonBack from "@/components/Buttons/ButtonBack";
 import HeaderAddMenuItem from "@/components/HeaderAddMenuItem";
 import MenuListBox from "@/components/MenuListBox";
 
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[1fr_20px] items-start justify-items-center min-h-screen p-3 sm:p-16 gap-16">
-			<main className="w-full flex flex-col gap-6 row-start-1 justify-center items-start">
-				<ButtonBack
-					link="/"
-					name="Wróc do listy nawigacji"
-				/>
-				<HeaderAddMenuItem />
-				<MenuListBox />
-			</main>
-		</div>
+		<AppProvider>
+			<div className="grid grid-rows-[1fr_20px] items-start justify-items-center min-h-screen p-3 sm:p-16 gap-16">
+				<main className="w-full flex flex-col gap-6 row-start-1 justify-center items-start">
+					<ButtonBack
+						link="/"
+						name="Wróc do listy nawigacji"
+					/>
+					<HeaderAddMenuItem />
+					<MenuListBox />
+				</main>
+			</div>
+		</AppProvider>
 	);
 }
